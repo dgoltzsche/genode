cf100eca20f0c5e19ae0e6e9b843201eeff89a8a
Norman Feske <norman.feske@genode-labs.com>
2014-04-24 12:40:18 +0200

    Merge libc_vfs plugin into libc
    Issue #999

--- libports/lib/mk/libc_fuse_exfat.mk
+++ libports/lib/mk/libc_fuse_exfat.mk
@@ -4,7 +4,7 @@ EXFAT_DIR = $(REP_DIR)/contrib/$(EXFAT)
 SRC_C  = $(notdir $(EXFAT_DIR)/fuse/main.c)
 SRC_CC = init.cc
 
-LIBS   = libc libc_vfs libfuse libexfat
+LIBS   = libc libfuse libexfat
 
 vpath %.c $(EXFAT_DIR)/fuse
 vpath %.cc $(REP_DIR)/src/lib/exfat
